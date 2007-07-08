78faca8f49375a4a470f4f5551b16f5f45e03bfd
ollie.the.pie.lord <ollie.the.pie.lord@823218a8-ad34-0410-91fc-f9441fec9dcb>
2007-07-08 12:30:32 +0000

    Finally beginning work setting out some structures for the map system

---
+++ Map Editor/MapEditor.h
@@ -0,0 +1,45 @@
+struct tile
+{
+	int tile_number;
+};
+
+struct tile_sprite
+{
+	int ID;
+	Bitmap bmp;
+};
+
+struct area
+{
+	int x,y,w,h;
+};
+
+struct light
+{
+	int x,y, r,g,b, radius;
+};
+
+struct entity
+{
+	int x,y, ID;
+};
+
+struct map
+{
+	std::vector<area> blocking;
+	std::vector<light> lights;
+	std::vector<entity> ents;
+	std::vector<tile> tiles;
+	std::vector<area> safe;
+	int width, height;
+
+	map(const std::string &filename)
+	{
+		load();
+	}
+
+private:
+	void load(const std::string &filename)
+	{
+	}
+};
